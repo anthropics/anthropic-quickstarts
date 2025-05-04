e1830b1ec34f7b9f74251618992c16f3b9e706c1
Ryan Kiser <ryan@thekiserhouse.net>
2025-05-04 15:40:47 -0400

    Fix for #266

--- computer-use-demo/computer_use_demo/loop.py
+++ computer-use-demo/computer_use_demo/loop.py
@@ -219,8 +219,6 @@ def _maybe_filter_to_n_most_recent_images(
     )
 
     images_to_remove = total_images - images_to_keep
-    # for better cache behavior, we want to remove in chunks
-    images_to_remove -= images_to_remove % min_removal_threshold
 
     for tool_result in tool_result_blocks:
         if isinstance(tool_result.get("content"), list):
